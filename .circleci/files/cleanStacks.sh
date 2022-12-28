e23c0da6d3a9625d3d2d43d65816f85ba1189d85
Reham-Fahmy <64305358+Reham-Fahmy@users.noreply.github.com>
2022-12-28 22:26:50 +0200

    Update cleanStacks.sh

--- .circleci/files/cleanStacks.sh
+++ .circleci/files/cleanStacks.sh
@@ -2,7 +2,7 @@ flowID=$1
 
   echo "Deleting $flowID"
   echo "URL: s3://${flowID}"
-  aws s3 rm "s3://$flowID" --recursive
+  aws s3 rm "s3://${flowID}" --recursive
   aws cloudformation delete-stack --stack-name "${flowID}-be"
   aws cloudformation delete-stack --stack-name "${flowID}-fe"
 

faa91cc0bfc74603af47135dcd1d4fa043bce68f
Reham-Fahmy <64305358+Reham-Fahmy@users.noreply.github.com>
2022-12-28 18:44:13 +0200

    Update cleanStacks.sh

--- .circleci/files/cleanStacks.sh
+++ .circleci/files/cleanStacks.sh
@@ -10,13 +10,9 @@ echo "BUCKETNAME: $BUCKETNAME"
 if [[ $stack =~ "$flowID" ]]
 then
   echo "Keeping $stack because equal"
-elif [[ $stack =~ "production" ]]
+fi
+if [[ $stack =! "$flowID" ]]
 then
-  echo "Keeping $stack because production stack"
-elif [[ $stack =~ "Initial" ]]
-then
-  echo "Keeping $stack because initial stack"
-else
   echo "Deleting $stack"
   echo "URL: s3://udapeople-$BUCKETNAME"
   aws s3 rb "s3://udapeople-$BUCKETNAME" --force
